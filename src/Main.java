dba622f1bfe374fb8a4834c598111dd974c862aa
ChynybekKazakbayev <chynybekkazakbaevuulu@gmail.com>
2019-08-30 19:51:02 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+      int [] numbers = new int[5];
+      numbers[0] = 11;
+      numbers[3] = 34;
+        System.out.println(numbers[3]);
+        System.out.println(numbers[2]);
+        System.out.println(numbers [0] + numbers [1]);
+
+        String names [] = new String[33];
+names[2] = "привет всем!";
+names[2] = " новое значение";
+// names [33] = "";
+
+        System.out.println(names[15]);
+
+        double[] digitsArray = {1.2, 45.6, 7.8, 1000.56};
+        System.out.println(digitsArray[3]);
+int [][] array = {{1,2},{3,4},{5,5}};
+        System.out.println(array[0][1]);
+        System.out.println();
+        System.out.println(array[2]);
+
+
+}
+}
